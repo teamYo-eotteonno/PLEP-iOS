f86c3fd036f0bbbb0385839cbb51b0f391bbd855
di351 <di35180@gmail.com>
2025-06-25 21:53:49 +0900

    2025.06.25/FlowKit, RxSwift 패키지 다운 및 rxswift 적용

---
+++ PLEP/PLEP/App/UI/Feature/Onboarding/Login/LoginViewModel.swift
@@ -0,0 +1,36 @@
+//
+//  LoginViewModel.swift
+//  PLEP
+//
+//  Created by 이다경 on 6/25/25.
+//
+
+import Foundation
+import RxSwift
+import RxCocoa
+
+class LoginViewModel: ObservableObject {
+    let email = BehaviorRelay<String>(value: "")
+    let password = BehaviorRelay<String>(value: "")
+    let loginTapped = PublishRelay<Void>()
+
+    @Published var isLoginEnabled = false
+    @Published var loginSuccess = false
+
+    private let disposeBag = DisposeBag()
+
+    init() {
+        Observable.combineLatest(email, password)
+            .map { !$0.0.isEmpty && !$0.1.isEmpty }
+            .bind(to: \.isLoginEnabled, on: self)
+            .disposed(by: disposeBag)
+
+        loginTapped
+            .withLatestFrom(Observable.combineLatest(email, password))
+            .flatMapLatest { email, password -> Observable<Bool> in
+                return AuthService.login(email: email, password: password)
+            }
+            .bind(to: \.loginSuccess, on: self)
+            .disposed(by: disposeBag)
+    }
+}
